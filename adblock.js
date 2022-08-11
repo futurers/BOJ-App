26f3cc55c3f4cc9a0155cea60552d5107110bb61
smartwe <githubdev.smartwe@gmail.com>
2022-08-11 11:19:02 +0900

    Update adblock.js

--- adblock.js
+++ adblock.js
@@ -2,8 +2,8 @@ function adblock()
 {
     while(document.getElementsByClassName('adsbygoogle')[0] != null)
     {
-        element = document.getElementsByClassName('adsbygoogle')
-        var i = 0
+        element = document.getElementsByClassName('adsbygoogle');
+        var i = 0;
         for(i; i < element.length; i++)
         {
             element[i].remove();
